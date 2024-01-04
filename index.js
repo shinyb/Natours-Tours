2f83ff75e313b934f1cbcf4f0ae7923f6b55c46f
Gloria Bradford <gloria_bradford2@yahoo.com>
2024-01-04 11:27:27 -0600

    Add Javascript file

---
+++ index.js
@@ -0,0 +1,7 @@
+/*const about = document.querySelector("#about");
+const benefits = document.querySelector("#benefits");
+const secTours = document.querySelector("#section-tours");
+const stories = document.querySelector("#stories");
+const book = document.querySelector("#book");
+
+console.log("Sometimes");*/
